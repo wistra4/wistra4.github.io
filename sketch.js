3ef9ebc2cce1fccc67bf64fb30f5350eb57ee925
wistra <1946keisuke.o@keio.jp>
2020-08-05 08:13:34 +0900

    fix: works3作品の内容を追加

--- sketch.js
+++ sketch.js
@@ -8,16 +8,4 @@ function setup() {
 
 function draw() {
     background(255);
-    for (var y = 0; y <= 1000; y = y + 500) {
-        for (var x = 0; x <= 1000; x = x + 500) {
-
-            
-            noFill();
-            stroke(255, 147, 206);
-            rotateX(frameCount * 0.01);
-            rotateY(frameCount * 0.01);
-            box(200, 200, 200);
-
-        }
-    }
 }
